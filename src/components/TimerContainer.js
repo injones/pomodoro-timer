b8e43098bb056b5e9b92dce1f26e88351e83eda3
Ieuan Jones <JonesIN@cardiff.ac.uk>
2018-10-17 17:26:43 +0100

    Refactored code and added documentation/comments

--- src/components/TimerContainer.js
+++ src/components/TimerContainer.js
@@ -2,42 +2,56 @@ import React, { Component } from 'react';
 
 
 export default class TimerContainer extends Component {
-    intervalID = 0;
-    state = {initial: new Date(), output: '', min: 0, sec: 0, isPaused: false, timeAmount: 1, message: ''};
+    intervalID = 0; // Global variable used to store setInterval ID
+    state = {
+        initial: new Date(),
+        output: '',
+        min: 0,
+        sec: 0,
+        isPaused: false,
+        timeAmount: 1,
+        message: ''
+    }; // initialised outside constructor to avoid state undefined errors
 
     constructor(props){
         super(props);
-
+        console.log(milliseconds2(0, 0));
+        console.log(milliseconds2(1, 0));
     }
 
+    /**
+     * Used to workout the target time in milliseconds from minutes, then set the state of the following
+     * variables using the time in milliseconds.
+     * - output
+     * - test
+     * Will only be called once after first mount
+     */
     componentDidMount(){
-        // const now = new Date().setMinutes(30, 0);
-        // const time = now.setSeconds(0).getTime();
-        // const now = new Date().getTime();
-        const mm = this.milliseconds(this.state.timeAmount, 0);
+        const mm = milliseconds(this.state.timeAmount, 0);
         const mins = Math.floor((mm % (1000 * 60 * 60)) / (1000 * 60));
         const secs = Math.floor((mm % (1000 * 60)) / 1000);
         this.setState({
-            distance: {},
             output: mins + "m " + secs + "s ",
             test: mm
         })
     }
 
-//     if (distance < 0){
-//     return (
-// <p>Reached end</p>
-// )
-// }
-
+    /**
+     * Toggles the 'isPaused' boolean declared within the Component state
+     */
     togglePause = () => {
         this.setState({
             isPaused: !this.state.isPaused
         })
     };
 
+    /**
+     * Handler for 'start' button, if the current time in milliseconds is zero the method assumes the countdown is to
+     * be restarted; thus resetting the state and starting the countdown, otherwise it will assume first time use
+     * and start the countdown with current state.
+     */
     handleStart = () => {
-        const target = this.milliseconds(this.state.timeAmount, 0);
+        const target = milliseconds(this.state.timeAmount, 0);
         const current = this.state.test;
         if (current === 0){
             this.setState({
@@ -52,27 +66,40 @@ export default class TimerContainer extends Component {
 
     setTimer = () => {
         this.setState({
-            test: this.milliseconds(this.state.timeAmount, 0)
+            test: milliseconds(this.state.timeAmount, 0)
         })
     };
 
+    /**
+     * Handler for time input from user, performs input validation to prevent runtime errors will run with default
+     * state, otherwise sets 'timeAmount' and 'test' variables to user input. ('input' should be in minutes)
+     * @param e - event for onChange
+     */
     handleInputChange = e => {
         const input = e.target.value;
-        if (input === null || input < 0 || isNaN(input)){
+        if (input === null || input < 1 || isNaN(input)){
             this.setState({
                 timeAmount: 0,
-                test: this.milliseconds(input, 0)
+                test: milliseconds(input, 0)
             })
         } else {
             this.setState({
                 timeAmount: input,
-                test: this.milliseconds(input, 0)
+                test: milliseconds(input, 0)
             })
         }
     };
 
+    /**
+     * Handler for 'reset' button, if the 'intervalID' variable is not equal to zero and the current time in
+     * milliseconds is not equal to the target time in milliseconds the method assumes countdown is currently active
+     * and will clearInterval of the currently active interval and reset the following Component state variables
+     * - test
+     * - isPaused
+     * - output
+     */
     handleReset = () => {
-        const target = this.milliseconds(this.state.timeAmount, 0);
+        const target = milliseconds(this.state.timeAmount, 0);
         const current = this.state.test;
         if (current !== target && this.intervalID !== 0){
             clearInterval(this.intervalID);
@@ -86,26 +113,24 @@ export default class TimerContainer extends Component {
         }
     };
 
-    milliseconds = (m, s) => ((60+m*60+s)*1000); //https://stackoverflow.com/a/51468455
 
+
+    /**
+     * Count down the 'test' Component state variable by 1000 milliseconds every second till zero.
+     */
     countDown = () => {
-        const init = new Date().getTime();
         this.intervalID = setInterval(() => {
             if (!this.state.isPaused){
-                const time = new Date().getTime();
-                const distance = time - init;
                 const n = this.state.test - 1000;
                 const tmins = Math.floor((n % (1000 * 60 * 60)) / (1000 * 60));
                 const tsecs = Math.floor((n % (1000 * 60)) / 1000);
                 if (n < 1000){
-                    console.log(this.state.test);
                     this.setState({
                         message: 'Time ran out!',
                         test: 0
                     });
                     clearInterval(this.intervalID);
                 }
-                console.log(tmins + "m " + tsecs + "s ");
                 this.setState({
                     test: n,
                     output: tmins + "m " + tsecs + "s "
@@ -151,3 +176,6 @@ export default class TimerContainer extends Component {
     }
 
 }
+
+const milliseconds = (m, s) => ((60+m*60+s)*1000-60000); //https://stackoverflow.com/a/51468455
+const milliseconds2 = (m, s) => ((60+m*60+s)*1000-60000);
